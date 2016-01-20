4ef337318328a98c285c104185754176dce27a12
Mike <hamanhhoang1102@gmail.com>
2016-01-21 01:23:38 +0800

    update

--- src/main/java/com/jd/prefix_sum/LargestSumContiguousSubarray_Kadane.java
+++ src/main/java/com/jd/prefix_sum/LargestSumContiguousSubarray_Kadane.java
@@ -1,14 +1,9 @@
 package com.jd.prefix_sum;
 
-public class LargestSumContiguousSubarray_Kadane {
+import org.junit.Assert;
+import org.junit.Test;
 
-  public static void main(String[] args) {
-    int[] a = {3, -1, -1, -1, -1, -1, 2, 0, 0, 0};
-    // int[] a = {-1, 3, -5, 4, 6, -1, 2, -7, 13, -3};
-    // int[] a = {-4, -3, -2, -1};
-    findMaxSubArray(a);
-    // System.out.println(maxSequenceSum(a));
-  }
+public class LargestSumContiguousSubarray_Kadane {
 
   public static int maxSequenceSum(int[] a) {
     int maxSoFar = a[0];
@@ -54,5 +49,77 @@ public class LargestSumContiguousSubarray_Kadane {
     System.out.println("Max sum         : " + maxSum);
     System.out.println("Max start index : " + maxStartIndex);
     System.out.println("Max end index   : " + maxEndIndex);
+  }
+
+  public static int maxSequenceSumIgnoreZero(int[] a) {
+    int maxSoFar = a[0];
+    int maxEndingHere = a[0];
+
+    for (int i = 1; i < a.length; i++) {
+      if (maxEndingHere < 0 || a[i] == 0)
+        maxEndingHere = a[i];
+      else
+        maxEndingHere += a[i];
+
+      if (maxEndingHere >= maxSoFar && maxEndingHere != 0 || maxSoFar == 0)
+        maxSoFar = maxEndingHere;
+    }
+    return maxSoFar;
+  }
+
+  @Test
+  public void test1() {
+    int[] a = {-3, -1, -1, -1, 0, -1, -2, 0, 0, 0};
+    Assert.assertEquals(0, maxSequenceSum(a));
+    Assert.assertEquals(-1, maxSequenceSumIgnoreZero(a));
+  }
+
+  @Test
+  public void test2() {
+    int[] a = {-3, -1, -1, 1, 0, 1, 4, 0, 0, 0};
+    Assert.assertEquals(6, maxSequenceSum(a));
+    Assert.assertEquals(5, maxSequenceSumIgnoreZero(a));
+  }
+
+  @Test
+  public void test3() {
+    int[] a = {3, -1, -1, -1, 0, -1, -2, 0, 0, 0};
+    Assert.assertEquals(3, maxSequenceSum(a));
+    Assert.assertEquals(3, maxSequenceSumIgnoreZero(a));
+  }
+
+  @Test
+  public void test4() {
+    int[] a = {0, -1, 0};
+    Assert.assertEquals(0, maxSequenceSum(a));
+    Assert.assertEquals(-1, maxSequenceSumIgnoreZero(a));
+  }
+
+  @Test
+  public void test5() {
+    int[] a = {0, 0, 0};
+    Assert.assertEquals(0, maxSequenceSum(a));
+    Assert.assertEquals(0, maxSequenceSumIgnoreZero(a));
+  }
+
+  @Test
+  public void test6() {
+    int[] a = {3, 4, 0, 1, 2};
+    Assert.assertEquals(10, maxSequenceSum(a));
+    Assert.assertEquals(7, maxSequenceSumIgnoreZero(a));
+  }
+
+  @Test
+  public void test7() {
+    int[] a = {-1, 0, 0};
+    Assert.assertEquals(0, maxSequenceSum(a));
+    Assert.assertEquals(-1, maxSequenceSumIgnoreZero(a));
+  }
+
+  @Test
+  public void test8() {
+    int[] a = {0, 0, -1};
+    Assert.assertEquals(0, maxSequenceSum(a));
+    Assert.assertEquals(-1, maxSequenceSumIgnoreZero(a));
   }
 }
