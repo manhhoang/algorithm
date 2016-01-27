1de596c35df788b9775c2d7d4863c9d04b2b5bcb
Mike Ha <hamanhhoang1102@gmail.com>
2016-01-27 10:18:48 +0800

    update

--- src/main/java/com/jd/linked_list/RemoveDuplicatedNodeLinkedList.java
+++ src/main/java/com/jd/linked_list/RemoveDuplicatedNodeLinkedList.java
@@ -1,8 +1,5 @@
 package com.jd.linked_list;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,6 +8,7 @@ public class RemoveDuplicatedNodeLinkedList {
   private static class LinkedList {
     int val;
     LinkedList next;
+    int len;
 
     LinkedList(int val) {
       this.val = val;
@@ -19,13 +17,16 @@ public class RemoveDuplicatedNodeLinkedList {
   }
 
   public static LinkedList removeDuplicates(LinkedList head) {
+    int len = 1;
     for (LinkedList x = head; x != null && x.next != null;) {
       if (x.val == x.next.val) {
         x.next = x.next.next;
       } else {
         x = x.next;
+        len++;
       }
     }
+    head.len = len;
     return head;
   }
 
@@ -42,53 +43,55 @@ public class RemoveDuplicatedNodeLinkedList {
     return head;
   }
 
-  public static Object[] solution(int[] arr) {
+  public static int[] solution(int[] arr) {
     LinkedList list = null;
     for (int i = 0; i < arr.length; i++) {
       list = insert(list, arr[i]);
     }
 
     LinkedList output = removeDuplicates(list);
-    List<Integer> ans = new ArrayList<>();
+    int[] ans = new int[output.len];
+    int i = 0;
     while (output != null) {
-      ans.add(output.val);
+      ans[i] = output.val;
       output = output.next;
+      i++;
     }
-    return ans.toArray();
+    return ans;
   }
 
   @Test
   public void test1() {
-    Assert.assertArrayEquals(new Object[] {1, 2, 4, 5}, solution(new int[] {1, 2, 2, 4, 5}));
+    Assert.assertArrayEquals(new int[] {1, 2, 4, 5}, solution(new int[] {1, 2, 2, 4, 5}));
   }
 
   @Test
   public void test2() {
-    Assert.assertArrayEquals(new Object[] {5, 2, 1}, solution(new int[] {5, 2, 1}));
+    Assert.assertArrayEquals(new int[] {5, 2, 1}, solution(new int[] {5, 2, 1}));
   }
 
   @Test
   public void test3() {
-    Assert.assertArrayEquals(new Object[] {1}, solution(new int[] {1}));
+    Assert.assertArrayEquals(new int[] {1}, solution(new int[] {1}));
   }
 
   @Test
   public void test4() {
-    Assert.assertArrayEquals(new Object[] {1}, solution(new int[] {1, 1}));
+    Assert.assertArrayEquals(new int[] {1}, solution(new int[] {1, 1}));
   }
 
   @Test
   public void test5() {
-    Assert.assertArrayEquals(new Object[] {3, 1}, solution(new int[] {3, 3, 1}));
+    Assert.assertArrayEquals(new int[] {3, 1}, solution(new int[] {3, 3, 1}));
   }
 
   @Test
   public void test6() {
-    Assert.assertArrayEquals(new Object[] {1}, solution(new int[] {1, 1, 1, 1}));
+    Assert.assertArrayEquals(new int[] {1}, solution(new int[] {1, 1, 1, 1}));
   }
 
   @Test
   public void test7() {
-    Assert.assertArrayEquals(new Object[] {1, 2}, solution(new int[] {1, 1, 2, 2}));
+    Assert.assertArrayEquals(new int[] {1, 2}, solution(new int[] {1, 1, 2, 2}));
   }
 }
