cdf152a7a6d946efb3abc7c09260f6393653ff4b
Mike Ha <hamanhhoang1102@gmail.com>
2016-03-17 15:04:50 +0800

    update

---
+++ src/main/java/com/jd/recursive_programming/TotalPositiveInteger.java
@@ -0,0 +1,36 @@
+package com.jd.recursive_programming;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TotalPositiveInteger {
+
+  public int solution(int n) {
+    if (n >= 0) {
+      int total = solution(n - 1);
+      return n + total;
+    } else {
+      return 0;
+    }
+  }
+
+  @Test
+  public void test1() {
+    Assert.assertEquals(3, solution(2));
+  }
+
+  @Test
+  public void test2() {
+    Assert.assertEquals(6, solution(3));
+  }
+
+  @Test
+  public void test3() {
+    Assert.assertEquals(15, solution(5));
+  }
+
+  @Test
+  public void test4() {
+    Assert.assertEquals(45, solution(9));
+  }
+}
