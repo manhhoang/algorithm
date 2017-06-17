1c089dadf993559433b390ae1095cf6214655e65
Manh Hoang <hamanhhoang1102@gmail.com>
2017-06-17 09:29:44 +0200

    added Array2D

--- src/main/java/com/algo/data_structure/array/Array2D.java
+++ src/main/java/com/algo/data_structure/array/Array2D.java
@@ -1,5 +1,24 @@
 package com.algo.data_structure.array;
 
+/*
+Imagine we have an image. We’ll represent this image as a simple 2D array where every pixel is a 1 or a 0.
+The image you get is known to have a single rectangle of 0s on a background of 1s.
+Write a function that takes in the image and returns the coordinates of the rectangle -- either top-left and bottom-right; or top-left, width, and height.
+
+Here are sample images using Java.
+
+// Java
+int[][] image = {
+  {1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 0, 1},
+  {1, 1, 1, 0, 0, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1}
+};
+
+3,2 3 2
+
+*/
 public class Array2D {
 
     static int[][] image = {
@@ -48,7 +67,7 @@ public class Array2D {
                 h = i - y;
             }
         }
-        System.out.println(x + " " + y + " " + w + " " + h);
+        System.out.println(x + "," + y + " " + w + " " + h);
     }
 
     public static void main(String[] args) {
