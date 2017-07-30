4a642afb11a762d7d02572a4349634bd93912c8a
Manh Hoang <hamanhhoang1102@gmail.com>
2017-07-30 09:45:46 +0200

    updated

---
+++ src/main/java/com/algo/data_structure/queue/Deque.java
@@ -0,0 +1,8 @@
+package com.algo.data_structure.queue;
+
+/*
+A double-ended queue (dequeue, often abbreviated to deque) is an abstract data type that generalizes a queue,
+for which elements can be added to or removed from either the front (head) or back (tail)
+ */
+public class Deque {
+}
