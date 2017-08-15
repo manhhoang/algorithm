dc9ae4c474c9ca1de6e2b43aed534b7923e396b0
Nguyen Thi Thu <thunguyen0705@gmail.com>
2017-08-15 20:46:27 +0200

    udpated

--- src/main/java/com/algo/data_structure/hash_map/MajorityElement.java
+++ src/main/java/com/algo/data_structure/hash_map/MajorityElement.java
@@ -1,31 +1,29 @@
 package com.algo.data_structure.hash_map;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
- *
+ * <p>
  * Input:
  * {1, 1, 2, 2, -3, -3, -3, -3, -3}
  * {1, 1, 2, 2, -3, -3, -3, -3}
- *
+ * <p>
  * Output:
  * -3
  * No
- *
  */
 public class MajorityElement {
 
     public static String majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++) {
-           int count = map.getOrDefault(nums[i], 0);
-           count++;
-           map.put(nums[i], count);
-           if(count * 2 > nums.length) {
-               return String.valueOf(nums[i]);
-           }
+        Map<Integer, Integer> map = new ConcurrentHashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.putIfAbsent(nums[i], 0);
+            map.merge(nums[i], 1, Integer::sum);
+            if (map.get(nums[i]) * 2 > nums.length) {
+                return String.valueOf(nums[i]);
+            }
         }
 
         return "No";
