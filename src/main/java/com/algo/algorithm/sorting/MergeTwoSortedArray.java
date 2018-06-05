94b5c855bc69e4fbbde8f7ab2ada356f13f82e1f
Manh Hoang <hamanhhoang1102@gmail.com>
2018-06-06 00:15:50 +1200

    updated

--- src/main/java/com/algo/algorithm/sorting/MergeTwoSortedArray.java
+++ src/main/java/com/algo/algorithm/sorting/MergeTwoSortedArray.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class MergeTwoSortedArray {
 
     public static int[] solution(int[] first, int[] second) {
-        int[] result = new int[first.length + second.length];
+        int[] merge = new int[first.length + second.length];
         int i = 0;
         int j = 0;
         int m = 0;
@@ -14,18 +14,18 @@ public class MergeTwoSortedArray {
         // Compare elements at i and j
         while (i < first.length && j < second.length) {
             if (first[i] < second[j]) {
-                result[m] = first[i];
+                merge[m] = first[i];
                 i++;
             } else {
-                result[m] = second[j];
+                merge[m] = second[j];
                 j++;
             }
             m++;
         }
         // Copy remaining elements
-        System.arraycopy(first, i, result, m, first.length - i);
-        System.arraycopy(second, j, result, m, second.length - j);
-        return result;
+        System.arraycopy(first, i, merge, m, first.length - i);
+        System.arraycopy(second, j, merge, m, second.length - j);
+        return merge;
     }
 
     @Test
@@ -37,7 +37,6 @@ public class MergeTwoSortedArray {
     public void test2() {
         Assert.assertArrayEquals(new int[]{1, 1, 1, 2, 3, 3, 4, 5}, solution(new int[]{1, 1, 3, 5}, new int[]{1, 2, 3, 4}));
     }
-
 
     @Test
     public void test3() {
